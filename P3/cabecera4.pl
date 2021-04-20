a1a72b8bef7ff0d753e206c75f9fa420fd33880a
elenacano <elenita.canocas@gmail.com>
2021-04-20 22:02:58 +0200

    Ejercicio 4 funciona perfecto

---
+++ P3/cabecera4.pl
@@ -0,0 +1,197 @@
+write_log(S) :- open('error_logs.txt', append, Out), write(Out, S), write(Out, '\n'), close(Out).
+
+/***************
+* EJERCICIO 1. sum_pot_prod/4
+*
+*	ENTRADA:
+*		X: Vector de entrada de numeros de valor real.
+*		Y: Vector de entrada de numeros de valor real.
+*		Potencia: Numero de valor entero, potencia.
+*	SALIDA:
+*		Resultado: Numero de valor real resultado de la operacion sum_pot_prod.
+*
+****************/
+
+sum_pot_prod(X, Y, Potencia, Resultado) :- print('Error. Este ejercicio no esta implementado todavia.'), !, fail.
+
+/***************
+* EJERCICIO 2. segundo_penultimo/3
+*
+*       ENTRADA:
+*               L: Lista de entrada de numeros de valor real.
+*       SALIDA:
+*               X : Numero de valor real. Segundo elemento.
+*		Y : Numero de valor real. Penultimo elemento.
+*
+****************/
+segundo_penultimo(L, X, Y) :- print('Error. Este ejercicio no esta implementado todavia.'), !, fail.
+
+/***************
+* EJERCICIO 3. sublista/5
+*
+*       ENTRADA:
+*		L: Lista de entrada de cadenas de texto.
+*		Menor: Numero de valor entero, indice inferior.
+*		Mayor: Numero de valor entero, indice superior.
+*		E: Elemento, cadena de texto.
+*       SALIDA:
+*		Sublista: Sublista de salida de cadenas de texto.
+*
+****************/
+
+
+%Calculamos la sublista
+calcula_sublista([X|_],1,1,E,[X]).
+calcula_sublista([X|Xs],1,K,E,[X|Ys]) :- K>1, K1 is K-1, calcula_sublista(Xs,1, K1,E,Ys).
+calcula_sublista([_|Xs],I,K,E,Ys)     :- I>1, I1 is I-1, K1 is K-1, calcula_sublista(Xs, I1, K1,E,Ys).
+
+%Comprobamos  que el elemento este en la sublista calculada
+check(E, Ys) :- not(member(E,Ys)), write_log('ERROR 3.1 Elemento').
+
+%sublista(L,Menor, Mayor, E, Sublista).
+sublista(L,Menor, Mayor, E, Sublista) :- Mayor<Menor, write_log('ERROR 3.2 Indices.'), !, fail.
+sublista(L,Menor, Mayor, E, Sublista) :- calcula_sublista(L,Menor, Mayor, E, Sublista), not(check(E, Sublista)).
+
+
+/***************
+* EJERCICIO 4. espacio_lineal/4
+*
+*       ENTRADA:
+*               Menor: Numero de valor entero, valor inferior del intervalo.
+*               Mayor: Nurange(I,I,[I]).
+range(I,K,[I|L]) :- I < K, I1 is I + 1, range(I1,K,L).mero de valor entero, valor superior del intervalo.
+*               Numero_elementos: Numero de valor entero, numero de valores de la rejilla.
+*       SALIDA:
+*               Rejilla: Vector de numeros de valor real resultante con la rejilla.
+*
+****************/
+%espacio_lineal(Menor, Mayor, Numero_elementos, Rejilla)
+
+calcula_espacio_lineal(_,Menor,Numero_elementos,[Menor],Numero_elementos).
+calcula_espacio_lineal(Menor,Mayor,Numero_elementos,[Menor|Rejilla],Paso) :- Paso<Numero_elementos, Paso2 is Paso+1, Max is Mayor*1.0, Min is Menor+(Mayor/(Numero_elementos-1)), calcula_espacio_lineal(Min,Max,Numero_elementos,Rejilla, Paso2).
+
+espacio_lineal(Menor, Mayor, Numero_elementos, Rejilla) :- Mayor<Menor, write_log('ERROR 4.1 Indices.'), !, fail.
+espacio_lineal(Menor, Mayor, Numero_elementos, Rejilla) :- calcula_espacio_lineal(Menor,Mayor,Numero_elementos,Rejilla,1).
+
+/***************
+* EJERCICIO 5. normalizar/2
+*
+*       ENTRADA:
+*		Distribucion_sin_normalizar: Vector de numeros reales de entrada. Distribucion sin normalizar.
+*       SALIDA:
+*		Distribucion: Vector de numeros reales de salida. Distribucion normalizada.
+*
+****************/
+normalizar(Distribucion_sin_normalizar, Distribucion) :- print('Error. Este ejercicio no esta implementado todavia.'), !, fail.
+
+/***************
+* EJERCICIO 6. divergencia_kl/3
+*
+*       ENTRADA:
+*		D1: Vector de numeros de valor real. Distribucion.
+*		D2: Vector de numeros de valor real. Distribucion.
+*       SALIDA:
+*		KL: Numero de valor real. Divergencia KL.
+*
+****************/
+divergencia_kl(D1, D2, KL) :- print('Error. Este ejercicio no esta implementado todavia.'), !, fail.
+
+/***************
+* EJERCICIO 7. producto_kronecker/3
+*
+*       ENTRADA:
+*		Matriz_A: Matriz de numeros de valor real.
+*		Matriz_B: Matriz de numeros de valor real.
+*       SALIDA:
+*		Matriz_bloques: Matriz de bloques (matriz de matrices) de numeros reales.
+*
+****************/
+producto_kronecker(Matriz_A, Matriz_B, Matriz_bloques) :- print('Error. Este ejercicio no esta implementado todavia.'), !, fail.
+
+/***************
+* EJERCICIO 8a. distancia_euclidea/3
+*
+*       ENTRADA:
+*               X1: Vector de numeros de valor real.
+*               X2: Vector de numeros de valor real.
+*       SALIDA:
+*               D: Numero de valor real. Distancia euclidea.
+*
+****************/
+distancia_euclidea(X1, X2, D) :- print('Error. Este ejercicio no esta implementado todavia.'), !, fail.
+
+/***************
+* EJERCICIO 8b. calcular_distancias/3
+*
+*       ENTRADA:
+*               X_entrenamiento: Matriz de numeros de valor real donde cada fila es una instancia representada por un vector.
+*               X_test: Matriz de numeros de valor real donde cada fila es una instancia representada por un vector. Instancias sin etiquetar.
+*       SALIDA:
+*               Matriz_resultados: Matriz de numeros de valor real donde cada fila es un vector con la distancia de un punto de test al conjunto de entrenamiento X_entrenamiento.
+*
+****************/
+calcular_distancias(X_entrenamiento, X_test, Matriz_resultados) :- print('Error. Este ejercicio no esta implementado todavia.'), !, fail.
+
+/***************
+* EJERCICIO 8c. predecir_etiquetas/4
+*
+*       ENTRADA:
+*               Y_entrenamiento: Vector de valores alfanumericos de una distribucion categorica. Cada etiqueta corresponde a una instancia de X_entrenamiento.
+*               K: Numero de valor entero.
+*               Matriz_resultados: Matriz de numeros de valor real donde cada fila es un vector con la distancia de un punto de test al conjunto de entrenamiento X_entrenamiento.
+*       SALIDA:
+*               Y_test: Vector de valores alfanumericos de una distribucion categorica. Cada etiqueta corresponde a una instancia de X_test.
+*
+****************/
+predecir_etiquetas(Y_entrenamiento, K, Matriz_resultados, Y_test) :- print('Error. Este ejercicio no esta implementado todavia.'), !, fail.
+
+/***************
+* EJERCICIO 8d. predecir_etiqueta/4
+*
+*       ENTRADA:
+*               Y_entrenamiento: Vector de valores alfanumericos de una distribucion categorica. Cada etiqueta corresponde a una instancia de X_entrenamiento.
+*               K: Numero de valor entero.
+*               Vec_distancias: Vector de valores reales correspondiente a una fila de Matriz_resultados.
+*       SALIDA:
+*               Etiqueta: Elemento de valor alfanumerico.
+*
+****************/
+predecir_etiqueta(Y_entrenamiento, K, Vec_distancias, Etiqueta) :- print('Error. Este ejercicio no esta implementado todavia.'), !, fail.
+
+/***************
+* EJERCICIO 8e. calcular_K_etiquetas_mas_relevantes/4
+*
+*       ENTRADA:
+*               Y_entrenamiento: Vector de valores alfanumericos de una distribucion categorica. Cada etiqueta corresponde a una instancia de X_entrenamiento.
+*               K: Numero de valor entero.
+*               Vec_distancias: Vector de valores reales correspondiente a una fila de Matriz_resultados.
+*       SALIDA:
+*		K_etiquetas: Vector de valores alfanumericos de una distribucion categorica.
+*
+****************/
+calcular_K_etiquetas_mas_relevantes(Y_entrenamiento, K, Vec_distancias, K_etiquetas) :- print('Error. Este ejercicio no esta implementado todavia.'), !, fail.
+
+/***************
+* EJERCICIO 8f. calcular_etiqueta_mas_relevante/2
+*
+*       ENTRADA:
+*               K_etiquetas: Vector de valores alfanumericos de una distribucion categorica.
+*       SALIDA:
+*               Etiqueta: Elemento de valor alfanumerico.
+*
+****************/
+calcular_etiqueta_mas_relevante(K_etiquetas, Etiqueta) :- print('Error. Este ejercicio no esta implementado todavia.'), !, fail.
+
+/***************
+* EJERCICIO 8g. k_vecinos_proximos/5
+*
+*       ENTRADA:
+*		X_entrenamiento: Matriz de numeros de valor real donde cada fila es una instancia representada por un vector.
+*		Y_entrenamiento: Vector de valores alfanumericos de una distribucion categorica. Cada etiqueta corresponde a una instancia de X_entrenamiento.
+*		K: Numero de valor entero.
+*		X_test: Matriz de numeros de valor real donde cada fila es una instancia representada por un vector. Instancias sin etiquetar.
+*       SALIDA:
+*		Y_test: Vector de valores alfanumericos de una distribucion categorica. Cada etiqueta corresponde a una instancia de X_test.
+*
+****************/
+k_vecinos_proximos(X_entrenamiento, Y_entrenamiento, K, X_test, Y_test) :- print('Error. Este ejercicio no esta implementado todavia.'), !, fail.
